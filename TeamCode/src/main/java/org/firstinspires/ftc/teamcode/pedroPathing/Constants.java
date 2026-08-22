c240af2d5093dc69c114f114ad05401512d5ee84
Tin Dao <tindao6644@gmail.com>
2026-08-22 17:31:40 -0400

    Team 27974 Update heading PIDF constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -15,7 +15,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(9.5)
-            .headingPIDFCoefficients(new PIDFCoefficients(2, 0.1, 0.2, 0.04))
+            .headingPIDFCoefficients(new PIDFCoefficients(3, 0.1, 0.2, 0.05))
             .forwardZeroPowerAcceleration(-37.4507960114)
             .lateralZeroPowerAcceleration(-43.0960090531);
 

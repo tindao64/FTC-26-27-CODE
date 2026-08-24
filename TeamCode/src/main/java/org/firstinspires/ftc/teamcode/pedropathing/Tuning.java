c63bc94196f7d27619a7307bf1ec27a0d981566b
Tin Dao <tindao6644@gmail.com>
2026-08-24 18:02:01 -0400

    Rename Tuning opmode to Pedro Pathing Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Tuning.java
@@ -41,7 +41,7 @@ import java.util.List;
  * @version 1.0, 6/26/2025
  */
 @Configurable
-@TeleOp(name = "Tuning", group = "Pedro Pathing")
+@TeleOp(name = "Pedro Pathing Tuning", group = "Tuning")
 public class Tuning extends SelectableOpMode {
     public static Follower follower;
 

2584491a83760cfec59de17b4692455b84513b67
Tin Dao <tindao6644@gmail.com>
2026-08-29 13:52:46 -0400

    Team 27964 Move from EncoderMecanumDrivetrain back to MecanumDrivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Team27964Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/Team27964Hardware.java
@@ -26,11 +26,7 @@ class Team27964Hardware extends Hardware {
 
     public PathConstraints pathConstraints = new PathConstraints(0.99, 100, 0.75, 1);
 
-    public EncoderMecanumDrivetrain.Constants driveConstants = (EncoderMecanumDrivetrain.Constants) new EncoderMecanumDrivetrain.Constants()
-            .setRunMode(DcMotor.RunMode.RUN_USING_ENCODER)
-            .setMotorPIDFCoefficients(new com.qualcomm.robotcore.hardware.PIDFCoefficients(6.0, 6.0, 2.0, 0.0))
-            .setMotorPowerScaling(1.0)
-            .setMaxAcceleration(1.0)
+    public MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1.0)
             .rightFrontMotorName("motorFR")
             .rightRearMotorName("motorBR")
@@ -62,7 +58,7 @@ class Team27964Hardware extends Hardware {
     public Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
-                .setDrivetrain(new EncoderMecanumDrivetrain(hardwareMap, driveConstants))
+                .mecanumDrivetrain(driveConstants)
                 .twoWheelLocalizer(localizerConstants)
                 .build();
     }

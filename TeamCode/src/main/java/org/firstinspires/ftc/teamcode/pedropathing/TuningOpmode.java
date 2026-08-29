7ee5d49baef4b31b6d85d08d6a9ffba96ba73733
Tin Dao <tindao6644@gmail.com>
2026-08-29 13:50:45 -0400

    Implement bulk reading for Tuning opmodes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedropathing/TuningOpmode.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.pedropathing;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.List;
+
+/**
+ * A little hacky wrapper class around Tuning to do bulk reading
+ */
+@TeleOp(name = "Pedro Pathing Tuning", group = "Tuning")
+public class TuningOpmode extends OpMode {
+    private final Tuning tuning = new Tuning();
+
+    private List<LynxModule> allHubs;
+
+    private void updateBulkReadCache() {
+        for (LynxModule hub : allHubs) {
+            hub.clearBulkCache();
+        }
+    }
+
+    @Override
+    public void init() {
+        // Transfer our hardware
+        tuning.gamepad1 = gamepad1;
+        tuning.gamepad2 = gamepad2;
+        tuning.telemetry = telemetry;
+        tuning.hardwareMap = hardwareMap;
+
+        // Set up bulk reading
+        allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL);
+        }
+        updateBulkReadCache();
+
+        tuning.init();
+    }
+
+    @Override
+    public void init_loop() {
+        updateBulkReadCache();
+        tuning.init_loop();
+    }
+
+    @Override
+    public void start() {
+        updateBulkReadCache();
+        tuning.start();
+    }
+
+    @Override
+    public void loop() {
+        updateBulkReadCache();
+        tuning.loop();
+    }
+
+    @Override
+    public void stop() {
+        tuning.stop();
+    }
+}

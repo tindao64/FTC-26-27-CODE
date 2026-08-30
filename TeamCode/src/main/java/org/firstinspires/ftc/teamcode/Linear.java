a5ac2ad9f09f5a61c29cfaac85f9cce7dbdd9247
berkadam18-hash <berkadam18@gmail.com>
2026-08-30 16:40:38 -0400

    Lesson Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Linear.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Linear extends LinearOpMode {
@@ -9,8 +10,11 @@ public class Linear extends LinearOpMode {
     Servo servo;
     @Override
     public void runOpMode() throws InterruptedException {
-        HardwareMap hardwareMap = this.hardwareMap
+        HardwareMap hardwareMap = this.hardwareMap;
+        CrServo.hardwareMap(CRServo.class,"CRServo");
+        Servo.hardwareMap(Servo.class,"Servo");
         waitForStart();
+
 
     }
 }

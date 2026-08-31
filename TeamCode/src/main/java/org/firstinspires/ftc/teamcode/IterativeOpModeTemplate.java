511de3e00499b5c9741555ab8092f8d108b9da1d
Tin Dao <tindao6644@gmail.com>
2026-08-31 09:59:44 -0400

    Update lessons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IterativeOpModeTemplate.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+@Disabled
+public class IterativeOpModeTemplate extends OpMode {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void loop() {
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModeTemplate.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp()
+@Disabled
+public class LinearOpModeTemplate extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+    }
+}

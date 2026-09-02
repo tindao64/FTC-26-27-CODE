a85c893423aec310d19454250a4ab3fcce2dbdfb
berkadam18-hash <berkadam18@gmail.com>
2026-09-02 10:28:18 -0400

    OpMode examples

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IterativeOpModeTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IterativeOpModeTemplate.java
@@ -7,12 +7,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp()
 @Disabled
 public class IterativeOpModeTemplate extends OpMode {
+
     @Override
     public void init() {
+        telemetry.addData("67: ", 67;
     }
 
     @Override
     public void loop() {
+        telemetry.addData("OPmode started",1 );
     }
 }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModeTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOpModeTemplate.java
@@ -1,16 +1,12 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp()
-@Disabled
+
 public class LinearOpModeTemplate extends LinearOpMode {
+
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+
     }
 }

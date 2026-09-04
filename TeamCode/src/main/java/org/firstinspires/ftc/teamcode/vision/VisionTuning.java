7c8bed30df4401fba6943030363b0aefabfed4f4
Tin Dao <tindao6644@gmail.com>
2026-09-04 19:48:22 -0400

    A tuning OpMode for vision tasks

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/VisionTuning.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.bylazar.camerastream.PanelsCameraStream;
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.teamspecific.HardwareManager;
+import org.firstinspires.ftc.vision.opencv.Circle;
+import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
+import org.opencv.core.Point;
+
+import java.util.List;
+
+@TeleOp(name = "Vision Tuning", group = "Tuning")
+public class VisionTuning extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Set up combined telemetry, to also show on panels
+        telemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);
+
+        Vision vision = HardwareManager.INSTANCE.createVision(hardwareMap);
+
+        // Set up panels camera feed
+        PanelsCameraStream.INSTANCE.startStream(vision.visionPortal, 5);
+
+        while (opModeInInit() || opModeIsActive()) {
+            // Get the blobs from the processor
+            List<ColorBlobLocatorProcessor.Blob> blobList = vision.colorBlobProcessor.getBlobs();
+
+            // Print the blobs on telemetry
+            telemetry.clearAll();
+            for (ColorBlobLocatorProcessor.Blob blob : blobList) {
+                Circle blobCircle = blob.getCircle();
+                Point center = vision.normalizeCoordinates(blob.getCircle().getCenter());
+
+                telemetry.addLine("Blob: centerX=" + center.x + "\tcenterY=" + center.y + "\tradius=" + blobCircle.getRadius());
+            }
+
+            telemetry.update();
+            sleep(250);
+        }
+
+        // clean up
+        vision.visionPortal.resumeLiveView();
+        PanelsCameraStream.INSTANCE.stopStream();
+    }
+}
